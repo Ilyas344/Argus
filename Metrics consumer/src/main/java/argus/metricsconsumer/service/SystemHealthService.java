0e3f971806c6c58656226023684b0e2b14d81165
Ilyas <xxxilyas92xxx@gmail.com>
2024-04-23 09:30:35 +0500

    add docs

--- Metrics consumer/src/main/java/argus/metricsconsumer/service/SystemHealthService.java
+++ Metrics consumer/src/main/java/argus/metricsconsumer/service/SystemHealthService.java
@@ -5,9 +5,24 @@ import argus.metricsconsumer.model.SystemHealth;
 import java.util.List;
 
 public interface SystemHealthService {
+    /**
+     *  Save metric in DB
+     * @param jsonString string metric
+     */
     void saveMetric(String jsonString);
 
+    /**
+     * Get metric by id
+     * @param id metric ID
+     * @return metric
+     */
+
     SystemHealth getMetric(Long id);
+
+    /**
+     * Get all metrics
+     * @return list of metrics
+     */
 
     List<SystemHealth> getAllMetric();
 }
